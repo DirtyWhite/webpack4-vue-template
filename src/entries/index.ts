dcd9ddf6e5845da1a63c337c352cef7744020a52
DirtyWhite <www.648283849@outlook.com>
2018-09-29 10:53:20 +0800

    commit

--- src/entries/index.ts
+++ src/entries/index.ts
@@ -1,9 +1,7 @@
 __webpack_public_path__ = window["__webpack_public_path__"]
 
 import Vue from 'vue'
-import root from '../module/base.vue'
-import yell from '../module/yell'
-console.log(Vue)
+import index from './index'
 new Vue({
-    render: h => h(root)
+    render: h => h(index)
 }).$mount('#app')
